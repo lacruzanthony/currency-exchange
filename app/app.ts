d3fa2d0d73d9c94923cc0d2805f360c18bdf7c29
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-22 16:47:19 -0500

    app/task-1: adding calculate router to the app

--- app/app.ts
+++ app/app.ts
@@ -1,16 +1,21 @@
 import express from "express";
 import { json } from "body-parser";
 import { getCurrenciesRouter } from "./routes/currencies-handler";
-import { getRateRoute } from "./routes/rate-handler";
+import { calculateRouter } from "./routes/calculate-handler";
+import { getRateRouter } from "./routes/rate-handler";
 import { errorHandler } from "./middlewares/error-handlers";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
 app.use(json());
 
 app.use(getCurrenciesRouter);
-app.use(getRateRoute);
-
+app.use(getRateRouter);
+app.use(calculateRouter);
+app.all("*", (_req, _res, _next) => {
+  throw new NotFoundError();
+});
 app.use(errorHandler);
 
 export { app };
