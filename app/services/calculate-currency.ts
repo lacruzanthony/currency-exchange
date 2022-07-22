6543130522156c3a30dddef6d4a8a3b87ae053f5
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-22 16:52:33 -0500

    app/task-1: calculate currency service added

---
+++ app/services/calculate-currency.ts
@@ -0,0 +1,37 @@
+import { fetchRate, RateResponse } from "./api-client";
+
+const REJECTED_STATUS = "rejected";
+
+interface IResult {
+  result: string[];
+}
+
+export const calculateCurrencies = async (
+  amount: number,
+  from: string,
+  to: string[]
+) => {
+  let response: IResult = { result: [] };
+  const rates: number[] = [];
+  const fetchets = [] as Array<Promise<RateResponse>>;
+
+  to.forEach((currency: string) => {
+    fetchets.push(fetchRate(`${from}${currency}`));
+  });
+
+  await Promise.allSettled(fetchets).then((results) =>
+    results.forEach((result) => {
+      const { status } = result;
+      if (status !== REJECTED_STATUS) {
+        const { value } = result;
+        rates.push(value.rate);
+      }
+    })
+  );
+
+  rates.forEach((rate, idx) => {
+    response.result.push(`${amount} ${from} is ${amount * rate} ${to[idx]}`);
+  });
+
+  return response;
+};
