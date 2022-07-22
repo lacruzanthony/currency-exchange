a99a4e99832f2b99a24d830965474d12663056b4
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-22 16:48:48 -0500

    app/task-1: /calculate endpoint created

---
+++ app/routes/calculate-handler.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { validateRequest } from "../middlewares/validate-request";
+import isValidCurrency from "../services/valid-currency";
+import { calculateCurrencies } from "../services/calculate-currencie";
+
+const router = express.Router();
+
+router.post(
+  "/calculate",
+  [
+    body("from").not().isEmpty().withMessage("'from' parameter is required"),
+    body("amount")
+      .not()
+      .isEmpty()
+      .isFloat({ gt: 0 })
+      .withMessage("'amount' must be greater than 0"),
+    body("from").custom(isValidCurrency),
+    body("to").not().isEmpty().withMessage("'to' parameter is required"),
+    body("to").custom(isValidCurrency),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    let { amount, from, to } = req.body;
+
+    const response = await calculateCurrencies(amount, from, to);
+
+    res.send(response);
+  }
+);
+
+export { router as calculateRouter };
