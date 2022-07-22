39d41e41e9f111906935c357ea8766d40f562bea
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-22 16:53:48 -0500

    app/task-1: valid currency service improved

--- app/services/valid-currency.ts
+++ app/services/valid-currency.ts
@@ -1,14 +1,25 @@
 import { CustomValidator } from "express-validator";
+import { BadRequestError } from "../errors/bad-request-error";
 import { fetchCurrencies } from "./api-client";
 
-const isValidCurrencie: CustomValidator = async (value: string | undefined) => {
+const isValidCurrencie: CustomValidator = async (
+  value: string | undefined | string[]
+) => {
   const currencies = await fetchCurrencies();
   if (value === undefined) {
     return Promise.resolve();
   }
 
-  if (!currencies.includes(value)) {
-    return Promise.reject();
+  if (Array.isArray(value)) {
+    value.forEach((currency) => {
+      if (!currencies.includes(currency)) {
+        throw new BadRequestError(`Currency ${currency} is not supported`);
+      }
+    });
+  } else {
+    if (!currencies.includes(value)) {
+      throw new BadRequestError(`Currency ${value} is not supported`);
+    }
   }
 
   return Promise.resolve();
