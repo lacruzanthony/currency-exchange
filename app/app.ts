5e66e61c7b4706c271be580b18900f5dc197b15f
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-21 23:31:27 -0500

    adding errorHandler

--- app/app.ts
+++ app/app.ts
@@ -2,13 +2,15 @@ import express from "express";
 import { json } from "body-parser";
 import { getCurrenciesRouter } from "./routes/currencies-handler";
 import { getRateRoute } from "./routes/rate-handler";
+import { errorHandler } from "./middlewares/error-handlers";
 
 const app = express();
 
 app.use(json());
 
 app.use(getCurrenciesRouter);
-
 app.use(getRateRoute);
+
+app.use(errorHandler);
 
 export { app };
