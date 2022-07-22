43973bc8b21875488bce1bea1a6449f0810afd63
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-21 23:32:28 -0500

    rate-handler route upgraded

--- app/routes/rate-handler.ts
+++ app/routes/rate-handler.ts
@@ -2,31 +2,28 @@ import { fetchRate } from "../services/api-client";
 import { body } from "express-validator";
 import express, { Request, Response } from "express";
 import { validateRequest } from "../middlewares/validate-request";
+import isValidCurrency from "../services/valid-currency";
 
 const router = express.Router();
 
 router.get(
   "/rate",
-  // [
-  //   body("from").notEmpty().withMessage("'from' parameter is required"),
-  //   body("to").notEmpty().withMessage("'to' parameter is required"),
-  // ],
-  // validateRequest,
+  [
+    body("from").not().isEmpty().withMessage("'from' parameter is required"),
+    body("from")
+      .custom(isValidCurrency)
+      .withMessage("Currency from is not supported"),
+    body("to").not().isEmpty().withMessage("'to' parameter is required"),
+    body("to")
+      .custom(isValidCurrency)
+      .withMessage("Currency to is not supported"),
+  ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    // const { from, to } = req.body;
-    // const errors: string[] = [];
-    // if (!from) {
-    //   errors.push("'from' parameter is required");
-    // }
-    // if (!to) {
-    //   errors.push("'to' parameter is required");
-    // }
-    // if (errors.length > 0) {
-    //   res.status(400).send({ errors });
-    //   throw new Error("Errors");
-    // }
-    // const { rate } = await fetchRate(`${from}${to}`);
-    // res.send({ rate });
+    const { from, to } = req.body;
+
+    const { rate } = await fetchRate(`${from}${to}`);
+    res.send({ rate });
   }
 );
 
