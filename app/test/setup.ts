8d2fdf8bd68c751b959817db09fe0f20db2f9256
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-27 11:42:24 -0500

    adding signin fn and preparing cache before all test

--- app/test/setup.ts
+++ app/test/setup.ts
@@ -1,1 +1,30 @@
+import { randomApiKey } from "../services/generate-key";
+
 jest.mock("../services/api-client.ts");
+
+import { cacheWrapper } from "../cache-wrapper";
+
+declare global {
+  var signin: () => string[];
+}
+
+beforeAll(() => {
+  cacheWrapper.connect();
+  const cache = cacheWrapper.client;
+  cache.set("currencies", ["USD", "JPY", "GBP", "EUR"]);
+});
+
+global.signin = () => {
+  const apiKey = randomApiKey(16);
+
+  // Build session Object. { user: {my_user: apikey} }.
+  const session = { user: { my_user: apiKey } };
+  // turn that session into JSON.
+  const sessionJSON = JSON.stringify(session);
+  // Take JSON and encode ig as base64.
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  return [`session=${base64}`];
+};
+
+export {};
