d88c75cd580d3b024705ee632a31d1deeaae7d7f
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-27 11:36:28 -0500

    calling cache client on index.ts

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,14 @@
 import { app } from "./app";
+import { cacheWrapper } from "./cache-wrapper";
+import { fetchCurrencies } from "./services/api-client";
 
 const start = async () => {
+  cacheWrapper.connect();
+
+  // Set cache with the valid currency at the startup.
+  const cache = cacheWrapper.client;
+  cache.set("currencies", await fetchCurrencies());
+
   app.listen(3000, () => {
     console.log("Listening on port 3000!!!!!!!!");
   });
