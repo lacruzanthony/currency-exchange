2a03906b37620e6f91d73f533ca044d7f3072f9b
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-27 11:40:49 -0500

    saving user on session cookie and fixing typos

--- app/routes/register-handler.ts
+++ app/routes/register-handler.ts
@@ -1,30 +1,34 @@
 import express, { Request, Response } from "express";
-import { body } from "express-validator";
-import NodeCache from "node-cache";
+import { param } from "express-validator";
 import { BadRequestError } from "../errors/bad-request-error";
 import { validateRequest } from "../middlewares/validate-request";
 import { randomApiKey } from "../services/generate-key";
 
-const TTL_DAYS = 30 * 3600;
-
 const router = express.Router();
-const cache = new NodeCache({ stdTTL: TTL_DAYS });
 
 router.post(
-  "/register",
-  [body("username").notEmpty().isString().withMessage("User must be valid")],
+  "/register/:userName",
+  [param("userName").isString().withMessage("User must be valid")],
   validateRequest,
   (req: Request, res: Response) => {
-    const { username } = req.body;
+    const { userName } = req.params;
 
-    if (!cache.has(username)) {
+    let userSession = req.session?.user;
+
+    // User doesn't exist or new user comming from request.
+    if (!userSession || Object.keys(userSession)[0] !== userName) {
       const apiKey = randomApiKey(16);
-      cache.set(username, apiKey);
+
+      req.session = {
+        user: { [userName]: apiKey },
+      };
+
+      userSession = req.session.user;
     } else {
       throw new BadRequestError("This user has already registered");
     }
 
-    res.status(201).send({ apiKey: cache.get(username) });
+    res.status(201).send({ apiKey: req.session?.user });
   }
 );
 
